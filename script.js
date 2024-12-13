f8636806a286019240210d226d739c26bed491f3
jamusya <aijamal241108@gmail.com>
2024-12-13 20:45:49 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,144 @@
+document.querySelectorAll(".choose-btn").forEach(button => {
+    button.addEventListener("click", function() {
+        alert("в корзину");
+    });
+});
+
+
+
+// document.getElementById("q").addEventListener("click", function(){
+//     alert("в корзину")
+// });
+
+// document.getElementById("w").addEventListener("click", function(){
+//     alert("в корзину")
+// });
+
+// document.getElementById("e").addEventListener("click", function(){
+//     alert("в корзину")
+// });
+
+// document.getElementById("r").addEventListener("click", function(){
+//     alert("в корзину")
+// });
+
+// document.getElementById("t").addEventListener("click", function(){
+//     alert("в корзину")
+// });
+
+// document.getElementById("y").addEventListener("click", function(){
+//     alert("в корзину")
+// });
+
+// document.getElementById("u").addEventListener("click", function(){
+//     alert("в корзину")
+// });
+
+// document.getElementById("i").addEventListener("click", function(){
+//     alert("в корзину")
+// });
+
+// document.getElementById("o").addEventListener("click", function(){
+//     alert("в корзину")
+// });
+
+// document.getElementById("p").addEventListener("click", function(){
+//     alert("в корзину")
+// });
+
+// document.getElementById("a").addEventListener("click", function(){
+//     alert("в корзину")
+// });
+
+
+
+// document.getElementById("s").addEventListener("click", function(){
+//     alert("в корзину")
+// });
+
+// document.getElementById("d").addEventListener("click", function(){
+//     alert("в корзину")
+// });
+
+// document.getElementById("f").addEventListener("click", function(){
+//     alert("в корзину")
+// });
+
+// document.getElementById("g").addEventListener("click", function(){
+//     alert("в корзину")
+// });
+
+// document.getElementById("h").addEventListener("click", function(){
+//     alert("в корзину")
+// });
+
+// document.getElementById("j").addEventListener("click", function(){
+//     alert("в корзину")
+// });
+
+// document.getElementById("k").addEventListener("click", function(){
+//     alert("в корзину")
+// });
+
+// document.getElementById("l").addEventListener("click", function(){
+//     alert("в корзину")
+// });
+
+// document.getElementById("z").addEventListener("click", function(){
+//     alert("в корзину")
+// });
+
+// document.getElementById("mutton").addEventListener("click", function(){
+//     alert("в корзину")
+// });
+// document.getElementById("mbutton").addEventListener("click", function(){
+//     alert("в корзину")
+// });
+
+// document.getElementById("mybuttonn").addEventListener("click", function(){
+//     alert("в корзину")
+// });
+
+// document.getElementById("mybuttoon").addEventListener("click", function(){
+//     alert("в корзину")
+// });
+
+// document.getElementById("mybuttton").addEventListener("click", function(){
+//     alert("в корзину")
+// });
+
+// document.getElementById("mybuutton").addEventListener("click", function(){
+//     alert("в корзину")
+// });
+
+// document.getElementById("mybbutton").addEventListener("click", function(){
+//     alert("в корзину")
+// });
+
+// document.getElementById("myybutton").addEventListener("click", function(){
+//     alert("в корзину")
+// });
+
+// document.getElementById("mmybutton").addEventListener("click", function(){
+//     alert("в корзину")
+// });
+
+// document.getElementById("mybuttonnn").addEventListener("click", function(){
+//     alert("в корзину")
+// }); 
+
+// document.getElementById("mybuttooon").addEventListener("click", function(){
+//     alert("в корзину")
+// });
+
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById("linkDodo").addEventListener("click", function() {
+        window.location.href = "https://dodopizza.kg/bishkek";
+    });
+    document.getElementById("linkInstagram").addEventListener("click", function() {
+        window.location.href = "https://www.instagram.com/dodopizzakg/?hl=ru";
+    });
+    document.getElementById("linkTikTok").addEventListener("click", function() {
+        window.location.href = "https://www.tiktok.com/@dodopizzakg";
+    });
+});
